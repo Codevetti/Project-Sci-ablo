f61ba32831c702a3debccc0197c85bf29d2f58cd
Salvetori <matt.salvetti@live.com>
2017-10-22 23:13:11 -0500

    Added the Movement Stuff
    Hoping this doesn't crash super hard

---
+++ Sci-ablo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Animator anim;
+    public Transform target;
+    public NavMeshAgent agent;
+    public Camera cam;
+
+    float dist;
+
+    Ray ray;
+    RaycastHit hit;
+    int layerMask;
+    int runId = Animator.StringToHash("Run");
+    // Use this for initialization
+    void Start () {
+
+        layerMask = LayerMask.GetMask("Floor");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit, layerMask))
+            {
+                target.position = hit.point;
+                agent.SetDestination(target.position);
+            }
+        }
+
+        dist = agent.remainingDistance;
+
+        if(dist!=Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+            //arrived at destination
+            anim.SetBool("Run", false);
+        }
+        else
+        {
+            if(!anim.GetBool(runId))
+                anim.SetBool("Run", true);
+        }
+        
+	}
+
+}
---
+++ Sci-ablo/Assets/Scripts/TargetOrbitScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetOrbitScript : MonoBehaviour {
+
+    public Transform camPivot;
+    Vector3 camPosition;
+    Vector3 camDirection;
+    public float camRadius;
+    public float camHeight;
+    public float camRotationSpeed;
+    float mouseX;
+    float mouseWheel;
+    // Use this for initialization
+    void Start () {
+        camPosition = new Vector3(-camPivot.forward.x * camRadius, camPivot.position.y + camHeight, -camPivot.forward.z * camRadius);
+        transform.position = camPosition;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        mouseX = Input.GetAxis("Mouse X");
+        
+        if (Input.GetKey(KeyCode.Mouse2))
+        {
+            if (mouseX < 0)
+            {
+                transform.RotateAround(camPivot.position, -transform.up, Time.deltaTime * camRotationSpeed);
+            }
+            else if (mouseX > 0)
+            {
+                // transform.Translate(Vector3.right * camRotationSpeed * Time.deltaTime);
+                transform.RotateAround(camPivot.position, transform.up, Time.deltaTime * camRotationSpeed);
+            }
+        }
+        
+    }
+
+    void Update()
+    {
+        mouseWheel = Input.GetAxis("Mouse ScrollWheel");
+        transform.LookAt(camPivot);
+        transform.Translate(Vector3.forward * mouseWheel* 30 * Time.deltaTime);
+    }
+}

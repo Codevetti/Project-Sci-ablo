aab9f943934e8f1ac769646b8d6645b68ef77329
Salvetori <matt.salvetti@live.com>
2017-11-13 12:40:52 -0600

    Need to Commit
    Added mining laser and some scripting stuff.

---
+++ Sci-ablo/Assets/Scripts/HammerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerScript : MonoBehaviour {
+
+    GameObject player;
+    PlayerEquipmentManager equipmentManager;
+
+	void Start()
+    {
+        //find the player and get the equipment script
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            equipmentManager = player.GetComponent<PlayerEquipmentManager>();
+        }
+    }
+
+    void Update()
+    {
+        //equip this weapon to the player.
+        //this wouldn't happen in update, but would happen when the weapon is put in the proper inventory slot.
+        if (!equipmentManager.hasWeapon)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                equipmentManager.hammer = transform;
+                equipmentManager.rend = transform.GetComponent<MeshRenderer>();
+                equipmentManager.isHammer = true;
+                equipmentManager.equipWeapon = true;
+                equipmentManager.isGun = false;
+            }
+        }
+    }
+
+}
---
+++ Sci-ablo/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour {
+
+    LineRenderer lineRen;
+    Vector3 origPos;
+    public float speed;
+
+    void Start()
+    {
+        lineRen = GetComponent<LineRenderer>();
+        origPos = Vector3.zero;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lineRen.SetPosition(0, origPos);
+            lineRen.SetPosition(1, origPos);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            //lineRen.SetPosition(1, lineRen.GetPosition(1) + Vector3.forward * speed * Time.deltaTime);
+            lineRen.SetPosition(1, Vector3.Lerp(lineRen.GetPosition(1), lineRen.GetPosition(0) + Vector3.forward * 50, 3 * speed * Time.deltaTime));
+        }
+        else
+        {
+            lineRen.SetPosition(0, Vector3.Lerp(lineRen.GetPosition(0), lineRen.GetPosition(1), 5 * speed * Time.deltaTime));
+        }
+	}
+}

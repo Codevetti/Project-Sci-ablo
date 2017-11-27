cd939e5ef89056e915f21a1c4664916ca6dfd3d7
BlakeWeimer <blake_weimer@yahoo.com>
2017-11-26 22:07:26 -0600

    Add files via upload
    Added health bar, works with the already existing healthManager script.

---
+++ Playerhealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerhealth : healthManager {
+
+	public GameObject healthBar;
+	public float percentHealth = 100;
+	public Vector3 healthBarVector = new Vector3 (1, 1, 1);
+
+	// Use this for initialization
+	void Start () {
+
+	playerHealth = 500;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		percentHealth = (playerHealth / 500);
+		healthBar.transform.localScale = new Vector3 (healthBarVector.x * percentHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+	}
+}

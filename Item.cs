033390d0c0456a22502a1fc262601b97cb7305bb
BlakeWeimer <blake_weimer@yahoo.com>
2017-11-26 21:08:06 -0600

    Add files via upload
    This is the scene containing the Inventory system and any scripts involved with it. Also included is the inventory slot.png,

---
+++ Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Item : ScriptableObject {
+
+	public Sprite sprite;
+	public bool isHammer;
+	public bool isGun;
+	public string name;
+	public bool isEquipped;
+}

22ceb864102d352db2760178c3dc08c579e3815c
ChadLiberto <chad.liberto@selu.edu>
2017-11-28 15:38:46 -0600

    Reconfigured map to work with updates. More enemy types with different related images for radar.

---
+++ Assets/Map/Scripts/MapArrow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("MiniMap/Map arrow")]
+public class MapArrow : MonoBehaviour {
+
+    private RectTransform _arrowRect;
+
+    public RectTransform ArrowRect
+    {
+        get
+        {
+            if (!_arrowRect)
+            {
+                _arrowRect = GetComponent<RectTransform>();
+                if (!_arrowRect)
+                {
+                    Debug.LogError("RectTransform not found. Image not attached.");
+                }
+            }
+
+            return _arrowRect;
+        }
+    }
+
+    public void rotate(Quaternion quat)
+    {
+        ArrowRect.rotation = quat;
+    }
+
+}
